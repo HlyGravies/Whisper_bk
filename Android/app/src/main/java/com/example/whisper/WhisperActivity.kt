24f9027f775c87d80eb2166a3e83308d843f2d02
mintie98 <thang3dinhat@gmail.com>
2024-04-22 15:36:15 +0900

    build:add WhisperActivity(Min)

---
+++ Android/app/src/main/java/com/example/whisper/WhisperActivity.kt
@@ -0,0 +1,11 @@
+package com.example.whisper
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WhisperActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_whisper)
+    }
+}
